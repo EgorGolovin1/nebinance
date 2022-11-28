6833f063c221a182c3db347ca5bdaf7455e311ac
Egor Golovin <egoooooooooorka@mail.ru>
2022-11-28 19:11:14 +0300

    delete comments

--- src/CryptoApp.js
+++ src/CryptoApp.js
@@ -4,8 +4,6 @@ import HeaderMenu from "./modules/components/header-menu/header-menu";
 import AddMenu from "./modules/components/add-menu/add-menu";
 import FilterMenu from "./modules/components/filter-menu/filter-menu";
 import ListItems from "./modules/components/list-items/list-items";
-// import ViewModalWindow from "./modules/components/modal-windows/view-modal";
-// import AddModalWindow from "./modules/components/modal-windows/add-modal";
 
 const CryptoApp = () => {
   return (
@@ -14,15 +12,6 @@ const CryptoApp = () => {
       <AddMenu />
       <FilterMenu />
       <ListItems />
-      {/* <AddModalWindow
-        // onRequestClose={closeAddModal}
-        isOpen={modalAddIsOpen}
-        closeAddModal={closeAddModal}
-      />
-      <ViewModalWindow
-        isOpen={modalViewIsOpen}
-        closeViewModal={closeViewModal}
-      /> */}
     </div>
   );
 };
